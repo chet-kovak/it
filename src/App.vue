8c09d81c78eae4bee8f530d00fb78a25f05cee31
pointblank <pointblank.connect@gmail.com>
2020-11-30 14:41:56 -0800

    beep

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="overflow-hidden pb-6">
+    <VueLoadingIndicator v-if="settings.loading" class="overlay big" />
+    <div v-else>
+      <Nav />
+      <router-view :key="$route.path" />
+      <vue-particles
+        class="vue-particles"
+        color="#dedede"
+        :particleOpacity="0.2"
+        :particlesNumber="50"
+        shapeType="circle"
+        :particleSize="0"
+        linesColor="#aaaaaa"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.2"
+        :linesDistance="250"
+        :moveSpeed="100"
+        :hoverEffect="false"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles> 
+    </div>
+ </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['settings'])
+  },
+  methods: {
+    ...mapActions(['init'])
+  },
+  async created() {
+    this.init();
+  }
+};
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav>
+    <div class="bg-topnav">
+      <div class="container-lg p-responsive d-flex">
+        <div class="flex-auto text-left">
+          <router-link :to="{ name: 'home' }">
+            <div class="row">
+                <img src="~/@/assets/logo.svg" class="logo" />
+              <h1 class="py-4">
+                $ITS
+              </h1>
+            </div>
+          </router-link>
+        </div>
+        <div class="py-4">
+          <a v-if="address" class="button button-info">
+            <span class="login-bullet mr-2 ml-n2" />
+            {{ name || shorten(address) }}
+          </a>
+          <a v-else class="button button-primary" @click="modalLoginOpen = true">
+            Connect wallet
+          </a>
+        </div>
+      </div>
+      <div class="pt-4 pb-10">
+        <h1 class="mx-auto container-md p-responsive ">ITERATION SYNDICATE</h1>
+      </div>
+    </div>
+    <div class="mb-4 mt-n4">
+      <div class="mainnav">
+        <router-link :to="{ name: 'home' }" class="button button-secondary">
+          ITS 
+        </router-link>
+        <router-link :to="{ name: 'about' }" class="button button-secondary">
+          ABOUT
+        </router-link>
+      </div>
+    </div>
+    <ModalLogin :open="modalLoginOpen" @close="modalLoginOpen = false" />
+  </nav>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import { shorten } from '@/helpers/utils';
+
+export default {
+  data() {
+    return {
+      modalLoginOpen: false
+    };
+  },
+  computed: mapState({
+    name: state => state.settings.name,
+    address: state => state.settings.address
+  }),
+  methods: {
+    shorten
+  }
+};
+</script>
